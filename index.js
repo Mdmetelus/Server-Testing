6a5c3749230ceb213515ddf584dd4c174ffe3628
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-31 18:47:34 -0500

    updated index

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
+
 const server = require('./server');
 
-
 const port = process.env.PORT || 9111;
-server.listen(port, function() {
+
+server.listen(port, () => {
   console.log(`\n=== Web API Listening on === \n=== http://localhost:${port} ===\n`);
 });
 
